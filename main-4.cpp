9c224f7b0480f24396618c9879edb7d56ab2706f
nouran68 <102485717+nouran68@users.noreply.github.com>
2022-11-16 19:47:15 +0100

    Add files via upload

---
+++ main-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "StringSet.h"
+using namespace std;
+int main() {
+//    string f = "file1";
+//
+//  StringSet s(fstream ("file.txt"));
+//  string text;
+//  StringSet s2(fstream("file2.txt") );
+    //s.add("tph");
+    //s.remove("i");
+    //cout<<s.numOfSet();
+//    StringSet set1("hi There i am salma");
+//    StringSet set2("hi TherY i am mohamed");
+//    cout<<set1.similarty(set2);
+}
