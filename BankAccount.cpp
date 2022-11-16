9ebb7a34984f8cede686ddef30895660d2afaa3a
nouran68 <102485717+nouran68@users.noreply.github.com>
2022-11-16 19:49:31 +0100

    Add files via upload

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,71 +1,49 @@
-//
-// Created by RTX on 11/14/2022.
-//
-
 #include "BankAccount.h"
 
-//********************************defult constructor to intialize balance with 0 L.E.***********************************
-BankAccount::BankAccount()
-{
-    balance = 0;
+BankAccount::BankAccount() {
+	// TODO - implement BankAccount::BankAccount
+	throw "Not yet implemented";
 }
-//*******************************constructor to set value of balance BY calling setBalance method***********************
-BankAccount::BankAccount(double balance)
-{
-    setBalance(balance);
-}
-//********************************fun to set value of balance***********************************************************
-void BankAccount::setBalance(double b)
-{
-    balance = b;
-}
-//**********************************fun to print value of balance ******************************************************
-double BankAccount::getBalance()
-{
-    return balance;
-}
-void BankAccount::setAccID(string id)
-{
-    accountID = id;
-}
-string BankAccount::getAccID()
-{
-    return accountID;
-}
-//**********************************fun to accept amount and subtract it from balance amount****************************
-void BankAccount::withdraw()
-{
-    double draw  ,Newbalance;
-    label:
-    cout << "Enter the amount you want to draw" << endl;
-    cin >> draw;
-    while(draw > balance)
-    {
-        cout << "Sorry. This is more than what you can withdraw." << endl;
-        goto label;
-    }
 
-    if (balance >= draw &&((balance-draw)>100) ) {
-        Newbalance = balance - draw;
-        balance = Newbalance;
-
-        cout << "Thank you."
-             << "\nAccount ID: " << accountID
-             << "\nNew Balance: " << balance << "\n";
-    }
-    else
-    {
-        cout<<"sorry, you can not take this amount of money!"<<endl;
-    }
+BankAccount::BankAccount(string balance) {
+	// TODO - implement BankAccount::BankAccount
+	throw "Not yet implemented";
 }
-//************************************fun to accept amount and add it to balance amount*********************************
-void BankAccount::setDeposit()
-{
-    double deposit;
-    cout << "Enter the amount you want to deposit" << endl;
-    cin >> deposit;
-    balance += deposit;
-    cout << "Thank you."
-         << "\nAccount ID: " << accountID
-         << "\nNew Balance: "<< balance << "\n";
+
+void BankAccount::setBalance(double b) {
+	this->balance = b;
+}
+
+void BankAccount::setAccountID(string id) {
+	// TODO - implement BankAccount::setAccountID
+	throw "Not yet implemented";
+}
+
+string BankAccount::getAccountID() {
+	// TODO - implement BankAccount::getAccountID
+	throw "Not yet implemented";
+}
+
+double BankAccount::getBalance() {
+	return this->balance;
+}
+
+void BankAccount::virtual_withdraw() {
+	// TODO - implement BankAccount::virtual withdraw
+	throw "Not yet implemented";
+}
+
+void BankAccount::virtual_setDeposit() {
+	// TODO - implement BankAccount::virtual setDeposit
+	throw "Not yet implemented";
+}
+
+void BankAccount::virtual_withdraw() {
+	// TODO - implement BankAccount::virtual withdraw
+	throw "Not yet implemented";
+}
+
+void BankAccount::virtual_setDeposit() {
+	// TODO - implement BankAccount::virtual setDeposit
+	throw "Not yet implemented";
 }
